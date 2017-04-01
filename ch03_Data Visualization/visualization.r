70db1ff70b7e909b009e365f2ce46d9ab19d13c4
ihong Chen <ehome4829@gmail.com>
2017-04-01 15:08:02 +0800

    完成3.7

--- ch03_Data Visualization/visualization.r
+++ ch03_Data Visualization/visualization.r
@@ -1,7 +1,8 @@
-
+######## visualization ################
 # packages 
 library(tidyverse)
-## visualization ##
+
+#### START ####
 # ex1 : bigger car engine use more fuel than small engine?
 
 View(mpg) # displ => engine size  , hwy => car fuel efficiency 
@@ -74,3 +75,110 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_wrap(~drv)
+
+#### 3.6 geom object #####
+
+ggplot(data=mpg) + 
+  geom_smooth(mapping=aes(x=displ,y=hwy)) 
+
+ggplot(data = mpg) + 
+  geom_point(mapping=aes(x=displ,y=hwy))
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+## combine two type
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x=displ,y=hwy)) +
+  geom_smooth(mapping = aes(x=displ,y=hwy)) # duplicated !!
+
+ggplot(data = mpg, mapping = aes(x=displ,y=hwy)) + 
+  geom_point() +
+  geom_smooth()
+  
+ggplot(data = mpg, mapping = aes(x=displ,y=hwy)) + 
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x=displ,y=hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth(data = filter(mpg,class=="subcompact"),se=FALSE)
+
+## 3.6.1 ex
+ggplot(data = mpg, mapping = aes(x=displ,y=hwy)) + 
+  geom_point(mapping = aes(color = class),show.legend = F) + 
+  geom_smooth() # 比較show.legend=T, F結果
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+ggplot() + 
+  geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg , mapping = aes(x=displ,y=hwy)) + 
+  geom_point() + 
+  geom_smooth(data = filter(mpg,class=="subcompact"),se=FALSE) + 
+  geom_smooth(data = filter(mpg,class=="compact"),se=FALSE)
+  
+#### 3.7 Statistical Transformation #####
+
+View(diamonds)
+
+ggplot(data=diamonds) + 
+  geom_bar(mapping = aes(x=cut))
+
+ggplot(data=diamonds) + 
+  stat_count(mapping = aes(x=cut))
+
+#  bar chart ->stat= "identity"
+demo <- tribble(
+  ~a,~b,
+  "bar_1",20,
+  "bar_2",30,
+  "bar_3",40
+  )
+ggplot(data = demo) +
+  geom_bar(mapping = aes(x = a,y=b), stat="identity")
+
+
+# bar chart percentage %
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x=cut,y=..prop..,group=1))
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity),position = 'fill')
+
+# stat summary 
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x=cut,y=depth),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median
+  )
+
+##### 3.7.1  ex #####
+?stat_summary # pointrange
+ggplot(data = diamonds,mapping=aes(x=cut,y=depth)) + 
+  geom_pointrange(aes(ymin=depth,ymax=depth))
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = ..prop..)) # without group=1
+
+## geom_col
+df <- data.frame(trt = c("a", "b", "c"), outcome = c(2.3, 1.9, 3.2))
+ggplot(df,aes(x=trt,y=outcome)) +
+  # geom_point()
+  geom_col()
+
+##### 3.8 Position Adjustment ######
+
