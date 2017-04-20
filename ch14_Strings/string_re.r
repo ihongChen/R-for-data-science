f22ca974195e89a9df374d376fea90ba7bbf9f2a
ihong Chen <ehome4829@gmail.com>
2017-04-20 22:16:33 +0800

    14.2 完成

---
+++ ch14_Strings/string_re.r
@@ -0,0 +1,108 @@
+library(tidyverse)
+
+
+# string ------------------------------------------------------------------
+
+string1 <- "This is a string" # recommended 
+string2 <- 'this is a string too'
+double_quote <- "\""
+single_quote <- '\''
+
+writeLines(single_quote)
+writeLines(double_quote)
+x <- c("\"","\\")
+writeLines(x)
+
+x<- "\u00b5"
+
+# 14.2.1 string length ----------------------------------------------------
+
+library(stringr)
+str_length(c("a","R for data science",NA))
+
+
+# 14.2.2 combine strings  -------------------------------------------------
+
+# str_c
+str_c("x","y")
+str_c("x","y","z")
+str_c("x","y",sep=",")
+str_c(x,"y",sep=",")
+
+x <- c("abc",NA)
+str_c(x,"y")
+
+x1 <- c("abc","def")
+str_c(x1,"y")
+
+str_c("|-", x, "-|")
+str_c("|-", str_replace_na(x), "-|")
+
+str_c("prefix-",c("a","b","c"),"-suffix")
+
+name<- "ihong"
+time_of_day <- "morning"
+birthday <- FALSE
+str_c(
+  "Good ",time_of_day," ",name,
+  if (birthday) " and Happy Birthday",
+  "."
+)
+
+str_c(c("x","y","z"),collapse = ", ")
+
+
+# 14.2.3 subseting strings------------------------------------------------------------------
+x <- c("Apple","Banana","Pear")
+str_sub(x,1,3)
+str_sub(x,-3,-1)
+str_sub("a",1,5)
+
+str_sub(x,1,1) <- str_to_lower(str_sub(x,1,1))
+x
+#### note 傳值 (call by value) ####
+x <- c(1,1,2)
+x2 <- x
+x2[3] <- 4
+x
+###################################
+
+
+# 14.2.4 locals -----------------------------------------------------------
+
+x <- c("apple", "eggplant", "banana")
+str_sort(x, locale = "en")  # English
+str_sort(x,locale="haw")
+
+
+# 14.2.5 ex ------------------------------------------------------------------
+# ex1
+paste(x,"y")
+paste0(x,"y") 
+
+str_c(x,"y",sep=" ") # same as : paste(x,"y")
+str_c(x,"y")
+
+# ex3
+str_length(x)
+str_length(c("哈嚕哈","你好嗎","a豬頭"))
+str_sub(x,1,3)
+
+middle_vec <- floor((str_length(x) +1)/2)
+str_sub(x,middle_vec,middle_vec)
+
+# ex4
+str_wrap()
+thanks_path <- file.path(R.home("doc"),"THANKS")
+thanks <-str_c(readLines(thanks_path),collapse = "\n")
+thanks
+cat(str_wrap(thanks),"\n")
+cat(str_wrap(thanks,width = 40),"\n")
+cat(str_wrap(thanks,width = 60, indent=2),"\n")
+cat(str_wrap(thanks,width = 60, exdent =2),"\n")
+
+## str_trim 
+str_trim("  String with trailing and leading white space\t")
+str_trim("\n\nString with trailing and leading white space\n\n")
+
+str_pad(c("a", "abc", "abcdef"), 10)
