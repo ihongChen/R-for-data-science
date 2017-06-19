04a17066907889d35fda2fa1e29e925298a401d9
ihong Sinopac <ehome4829@gmail.com>
2017-06-19 17:39:07 +0800

    加入hybrid練習

---
+++ ch_RealSample/Recommender-hybrid/hybrid-jester.r
@@ -0,0 +1,113 @@
+## create a hybrid recommender
+
+
+HybridRecommender <- function(..., weights = NULL) {
+  recommender <- list(...)
+  
+  if(is.null(weights)) weights <- rep(1, length(recommender))
+  else if(length(recommender) != length(weights)) stop("Number of recommender and length of weights does not agree!")
+  weights <- weights/sum(weights)
+  
+  if(!all(sapply(recommender, is, "Recommender"))) stop("Not all supplied models are of class 'Recommender'.")
+  
+  model <- list(recommender = recommender, weights = weights)
+  
+  predict <- function(model=NULL, newdata, n=10,
+                      data= NULL, type=c("topNList", "ratings", "ratingMatrix"), ...) {
+    
+    type <- match.arg(type)
+    
+    ## newdata are userid
+    if(is.numeric(newdata)) {
+      if(is.null(data) || !is(data, "ratingMatrix"))
+        stop("If newdata is a user id then data needes to be the training dataset.")
+      newdata <- data[newdata, , drop = FALSE]
+    }
+    
+    #if(ncol(newdata) != length(model$labels)) stop("number of items in newdata does not match model.")
+    
+    pred <- lapply(model$recommender, FUN = function(object)
+      object@predict(object@model, newdata, data=data, type="ratings", ...))
+    
+    ratings <- matrix(NA, nrow=nrow(newdata), ncol = ncol(newdata))
+    for(i in 1:nrow(pred[[1]])) {
+      ratings[i,] <- colSums(t(sapply(pred, FUN = function(p)
+        as(p[i,], "matrix"))) * model$weights, na.rm = TRUE)
+    }
+    
+    ratings <- as(ratings, "realRatingMatrix")
+    colnames(ratings) <- colnames(newdata)
+    
+    
+    if(type == "ratingMatrix")
+      stop("Hybrid cannot predict a complete ratingMatrix!")
+    
+    returnRatings(ratings, newdata, type, n)
+  }
+  
+  ## this recommender has no model
+  new("Recommender", method = "HYBRID",
+      dataType = "ratingMatrix",
+      ntrain = NA_integer_,
+      model = model,
+      predict = predict)
+}
+
+
+ri <- Recommender(data,'IBCF')
+ru <- Recommender(data[2:5,1:7],'UBCF')
+HybridRecommender(ri,ru)
+
+ri@model
+ru@model$data
+
+
+##### test: not the same dim U-I matrix with hybrid ###
+data("Jester5k")
+Jester_binary <- binarize(Jester5k,minRating=5)
+Jester_binary <- Jester_binary[rowCounts(Jester_binary) > 20]
+
+Jester_binary500 <- sample(Jester_binary,500)
+Jester_binary500_2 <- sample(Jester_binary,500)
+
+image(Jester_binary500,type="b")
+Jb1 <- Jester_binary500[1:100,]
+Jb2 <- Jester_binary500_2[2:200,]
+Jb1@data
+class(Jb1)
+recc1 <- Recommender(Jb1,'UBCF')
+recc2 <- Recommender(Jb2,'IBCF')
+
+hy12 <- HybridRecommender(recc1,recc2)
+predhy12 <- hy12@predict(hy12@model,Jester_binary500_2[1:10])
+
+colnames(Jester_binary500_2[5:10,5:10])
+
+predhy12@itemLabels
+predhy12@items
+predhy12@ratings
+
+predhy
+
+hy12@model
+
+
+###### evaluation #####
+
+eval_sets <- evaluationScheme(data = Jester_binary500_2,
+                              method = "cross-validation",
+                              k = 3,
+                              given = -1)
+
+eval_prediction <- predict(object = hy12,
+                           newdata = getData(eval_sets,"known"),
+                           n = 10, # item to recommend
+                           type = "topNList")
+eval_accuracy <- calcPredictionAccuracy(
+  x = eval_prediction,
+  data = getData(eval_sets, "unknown"),
+  byUser = FALSE,
+  given = 10)
+
+eval_accuracy
+
