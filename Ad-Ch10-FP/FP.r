f7912b8cd9a28af4bd8e71922019542ef878e97b
ihong Sinopac <ehome4829@gmail.com>
2017-06-30 17:29:52 +0800

    FP complete

--- Ad-Ch10-FP/FP.r
+++ Ad-Ch10-FP/FP.r
@@ -152,3 +152,157 @@ lapply(mtcars,pick(5))
 
 pick(5)(mtcars$mpg)
 
+
+
+# 10.4 Lists of functions -------------------------------------------------
+
+compute_mean <- list(
+  base = function(x) mean(x),
+  sum = function(x) sum(x)/length(x),
+  manual = function(x){
+    total <- 0
+    n <- length(x)
+    for (i in seq_along(x)){
+      total <- total + x[i]/n
+    }
+    total
+  }
+)
+
+
+x <- runif(1e5)
+system.time(compute_mean$base(x))
+system.time(compute_mean$sum(x))
+system.time(compute_mean$manual(x))
+
+lapply(compute_mean,function(f) f(x))
+
+call_fun <- function(f,...) f(...)
+lapply(compute_mean,call_fun,x)
+# time each function in list
+lapply(compute_mean,function(f) system.time(f(x)))
+
+#
+x <- 1:10 
+funs <- list(
+  sum = sum,
+  mean = mean,
+  median = median
+)
+
+lapply(funs,function(f) f(x))
+
+# how to remove missing data ?
+funs2 <- list( 
+  # not that good method #
+  sum = function(x) sum(x,na.rm=T),
+  mean = function(x) mean(x,na.rm=T),
+  median = function(x) median(x,na.rm=T)
+)
+lapply(funs2,function(f) f(x))
+
+lapply(funs, function(f) f(x,na.rm=T))
+
+
+# 10.4.1 ------------------------------------------------------------------
+## moving lists of functions to the global envir
+
+simple_tag <- function(tag){
+  force(tag)
+  function(...){
+    paste0("<",tag,">",paste0(...),"</",tag,">")
+  }
+}
+
+simple_tag('p')
+
+
+tags <- c("p",'b','i')
+html <- lapply(setNames(tags,tags),simple_tag)
+html$p("This is ",html$b("bold")," text.")
+
+##1 best way 
+with(html,p("This is ", b("bold"),"text."))
+
+# 2
+attach(html)
+p("This is ",b("bold")," text.")
+detach(html)
+
+
+# 3 copy to global environment
+
+list2env(html, environment())
+p("This is ",b("bold")," text.")
+rm(list = names(html),envir = environment())
+
+
+### eg. summary function ###
+
+field_names <- c('Min','Mean','Max')
+
+mySummary <- list(
+  my_min <- function(x) min(x),
+  my_median <- function(x) quantile(x,0.25),
+  my_mean <- function(x) mean(x)
+)
+x = 1:20
+lapply(mySummary,function(f) f(x))
+mySummary[[2]](1:10)
+
+
+
+# 10.5 Case study: numerical integration ----------------------------------
+
+# integrate sin(x) from 0 to pi ~ 2
+
+midpoint <- function(f,a,b){
+  (b-a) * f((a+b)/2)
+}
+
+trapezoid <- function(f,a,b){
+  (b-a) / 2*(f(a) + f(b))
+}
+# test 
+midpoint(sin,0,pi)
+trapezoid(sin,0,pi)
+
+
+midpoint_composite <- function(f,a,b,n=10){
+  points <- seq(a,b,length = n+1)
+  h <- (b-a) / n
+  
+  area <- 0
+  for (i in seq_len(n)){
+    area <- area + h * f((points[i] + points[i + 1])/2)
+  }
+  area
+}
+trapezoid_composite <- function(f, a, b, n = 10) {
+  points <- seq(a, b, length = n + 1)
+  h <- (b - a) / n
+  area <- 0
+  for (i in seq_len(n)) {
+    area <- area + h / 2 * (f(points[i]) + f(points[i + 1]))
+  }
+  area
+}
+
+midpoint_composite(sin,0,pi,n=10)
+midpoint_composite(sin,0,pi,n=100)
+trapezoid_composite(sin,0,pi,n=10)
+trapezoid_composite(sin,0,pi,n=100)
+## modify 
+
+composite <- function(f,a,b,n=10,rule){
+  points <- seq(a,b,length = n+1)
+  
+  area <- 0
+  for (i in seq_len(n)){
+    area <- area + rule(f,points[i],points[i+1])
+  }
+  area
+}
+
+composite(sin,0,pi,n=10,rule = midpoint)
+composite(sin,0,pi,n=10,rule = trapezoid)
